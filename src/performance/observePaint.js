6da8b01d3665e27c8275c38e9ce1034202df5d2b
woai3c <411020382@qq.com>
2021-10-08 21:15:27 +0800

    refactor: 调整代码位置，更加易读

--- src/performance/observePaint.js
+++ src/performance/observePaint.js
@@ -3,28 +3,28 @@ import { addCache } from '../utils/cache'
 import { lazyReportCache } from '../utils/report'
 
 export default function observePaint() {
-    if (isSupportPerformanceObserver()) {
-        const entryHandler = (list) => {        
-            for (const entry of list.getEntries()) {
-                if (entry.name === 'first-contentful-paint') {
-                    observer.disconnect()
-                }
+    if (!isSupportPerformanceObserver()) return
     
-                const json = entry.toJSON()
-                delete json.duration
-    
-                const reportData = {
-                    ...json,
-                    subType: entry.name,
-                    type: 'performance',
-                }
-
-                addCache(reportData)
-                lazyReportCache()
+    const entryHandler = (list) => {        
+        for (const entry of list.getEntries()) {
+            if (entry.name === 'first-contentful-paint') {
+                observer.disconnect()
             }
-        }
     
-        const observer = new PerformanceObserver(entryHandler)
-        observer.observe({ type: 'paint', buffered: true })
+            const json = entry.toJSON()
+            delete json.duration
+    
+            const reportData = {
+                ...json,
+                subType: entry.name,
+                type: 'performance',
+            }
+
+            addCache(reportData)
+            lazyReportCache()
+        }
     }
+    
+    const observer = new PerformanceObserver(entryHandler)
+    observer.observe({ type: 'paint', buffered: true })
 }
