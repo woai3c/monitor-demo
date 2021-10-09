eb705fe496d526726f33326f613c593292e61e50
woai3c <411020382@qq.com>
2021-10-09 16:28:06 +0800

    chore: 对错误数据进行上报

--- src/error/index.js
+++ src/error/index.js
@@ -1,4 +1,5 @@
 import { report } from '../utils/report'
+import config from '../config'
 
 export default function error() {
     // 捕获资源加载失败错误 js css img...
@@ -6,21 +7,48 @@ export default function error() {
         const target = e.target
         if (!target) return
 
-        let url = ''
         if (target.src || target.href) {
-            url = target.src || target.href
-            console.log(url)
+            const url = target.src || target.href
+            report({
+                url,
+                type: 'error',
+                subType: 'resource',
+                startTime: e.timeStamp,
+                html: target.outerHTML,
+                resourceType: target.localName,
+                paths: e.path.map(item => item.localName).filter(Boolean),
+                pageURL: window.location.href,
+            })
         }
     }, true)
 
     // 监听 js 错误
-    window.onerror = (msg, url, row, col, error) => {
-        console.log(msg, url, row, col, error)
-        console.log(error)
+    window.onerror = (msg, url, line, column, error) => {
+        report({
+            msg,
+            line,
+            column,
+            error: error.stack,
+            subType: 'js',
+            pageURL: url,
+            type: 'error',
+            startTime: performance.now(),
+        })
     }
 
     // 监听 promise 错误 缺点是获取不到列数据
     window.addEventListener('unhandledrejection', e => {
-        console.log(e.reason)
+        report({
+            reason: e.reason?.stack,
+            subType: 'promise',
+            type: 'error',
+            startTime: e.timeStamp,
+        })
     })
+
+    if (config.Vue) {
+        config.Vue.config.errorHandler = (err, vm, info) => {
+            console.log(err, vm, info)
+        }
+    }
 }
