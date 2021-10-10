fef6d5b902dc4c6545173ae5fe7461d2b2c2def1
woai3c <411020382@qq.com>
2021-10-10 23:32:19 +0800

    feat: 新增 vue router 变化时的信息统计

---
+++ src/behavior/onVueRouter.js
@@ -0,0 +1,27 @@
+import { lazyReportCache } from '../utils/report'
+
+export default function onVueRouter(router) {
+    router.beforeEach((to, from, next) => {
+        // 首次加载页面不用统计
+        if (!from.name) {
+            return next()
+        }
+
+        const data = {
+            params: to.params,
+            query: to.query,
+        }
+
+        lazyReportCache({
+            data,
+            name: to.name || to.path,
+            type: 'behavior',
+            subType: 'vueRouterChange',
+            startTime: performance.now(),
+            from: from.fullPath,
+            to: to.fullPath,
+        })
+
+        next()
+    })
+}
