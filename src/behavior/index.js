2b7d84ae485eee400e3a08cb087343c418bf9f72
woai3c <411020382@qq.com>
2021-10-10 23:31:57 +0800

    feat: 新增浏览器前端后退操作的事件监听和统计

--- src/behavior/index.js
+++ src/behavior/index.js
@@ -2,10 +2,18 @@ import pv from './pv'
 import pageAccessDuration from './pageAccessDuration'
 import pageAccessHeight from './pageAccessHeight'
 import onClick from './onClick'
+import config from '../config'
+import onVueRouter from './onVueRouter'
+import pageChange from './pageChange'
 
 export default function behavior() {
     pv()
     pageAccessDuration()
     pageAccessHeight()
     onClick()
+    pageChange()
+
+    if (config.vue?.router) {
+        onVueRouter(config.vue.router)
+    }
 }
---
+++ src/behavior/pageChange.js
@@ -0,0 +1,34 @@
+import { getPageURL } from '../utils/utils'
+import { lazyReportCache } from '../utils/report'
+
+export default function pageChange() {
+    let from = ''
+    window.addEventListener('popstate', () => {
+        const to = getPageURL()
+
+        lazyReportCache({
+            from,
+            to,
+            type: 'behavior',
+            subType: 'popstate',
+            startTime: performance.now,
+        })
+
+        from = to
+    }, true)
+
+    let oldURL = ''
+    window.addEventListener('hashchange', event => {
+        const newURL = event.newURL
+
+        lazyReportCache({
+            from: oldURL,
+            to: newURL,
+            type: 'behavior',
+            subType: 'hashchange',
+            startTime: performance.now,
+        })
+
+        oldURL = newURL
+    }, true)
+}
