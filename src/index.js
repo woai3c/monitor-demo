e1a76b1bf278b73caef51b062d0b0f091fcf7f23
tanguangzhi <tanguangzhi@shiqiao.com>
2021-10-10 15:16:22 +0800

    chore: 页面隐藏或关闭前将所有 cach 数据进行上报

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
 import error from './error/index'
 import performance from './performance/index'
 import { setConfig } from './config'
+import { onBeforeunload, onHidden } from './utils/utils'
+import { report } from './utils/report'
+import { getCache, clearCache } from './utils/cache'
 
 const monitor = {
     init(options = {}) {
         setConfig(options)
         error()
-        performance()
+        performance();
+
+        // 当页面进入后台或关闭前时，将所有的 cache 数据进行上报
+        [onBeforeunload, onHidden].forEach(fn => {
+            fn(() => {
+                const data = getCache()
+                if (data.length) {
+                    report(data, true)
+                    clearCache()
+                }
+            })
+        })
     },
 }
 
