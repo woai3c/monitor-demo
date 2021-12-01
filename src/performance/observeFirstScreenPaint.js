fffb7fa0e02171aea1822680a1c3a6e7102e89a0
woai3c <411020382@qq.com>
2021-12-01 19:43:51 +0800

    chore: 使用 getComputedStyle(node) 代替 node.style 来获取元素的 display 属性.

--- src/performance/observeFirstScreenPaint.js
+++ src/performance/observeFirstScreenPaint.js
@@ -110,14 +110,14 @@ function getRenderTime() {
 }
 
 function needToCalculate(node) {
+    // 隐藏的元素不用计算
+    if (window.getComputedStyle(node).display === 'none') return false
+    
     // 用于统计的图片不用计算
     if (node.tagName === 'IMG' && node.width < 2 && node.height < 2) {
         return false
     }
-
-    // 隐藏的元素不用计算
-    if (node.style.display === 'none') return false
-
+    
     return true
 }
 
